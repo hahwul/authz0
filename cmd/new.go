5437c4fc7876434081cde17afb1d592ffd1e01b4
hahwul <hahwul@gmail.com>
2022-01-11 01:27:53 +0900

    Add flag in newCmd

--- cmd/new.go
+++ cmd/new.go
@@ -6,6 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var name string
+var includeURLs, includeRoles string
+var successStatus string
+var failStatus, failRegex string
+var failSize int
+
 // newCmd represents the new command
 var newCmd = &cobra.Command{
 	Use:   "new",
@@ -17,4 +23,11 @@ var newCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(newCmd)
+	newCmd.PersistentFlags().StringVarP(&name, "name", "n", "", "Template name")
+	newCmd.PersistentFlags().StringVar(&includeURLs, "include-urls", "", "Include URLs from the file")
+	newCmd.PersistentFlags().StringVar(&includeRoles, "include-roles", "", "Include Roles from the file")
+	newCmd.PersistentFlags().StringVar(&successStatus, "policy-success-status", "", "Set success status policy")
+	newCmd.PersistentFlags().StringVar(&failStatus, "policy-fail-status", "", "Set fail status policy")
+	newCmd.PersistentFlags().StringVar(&failRegex, "policy-fail-regex", "", "Set fail regex policy")
+	newCmd.PersistentFlags().IntVar(&failSize, "policy-fail-size", 0, "Set fail size policy")
 }
