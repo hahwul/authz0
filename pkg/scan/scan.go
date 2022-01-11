36cd6887ab9ebcf4b23fc46a06708d9a37491038
hahwul <hahwul@gmail.com>
2022-01-12 01:36:02 +0900

    Update scan

--- pkg/scan/scan.go
+++ pkg/scan/scan.go
@@ -1,13 +1,17 @@
 package scan
 
+import "github.com/hahwul/authz0/pkg/authz0"
+
 type ScanArguments struct {
 	RoleName string
 	Cookie   string
 	Headers  []string
 }
 
-func Run(filename string, arguments ScanArguments), {
+func Run(filename string, arguments ScanArguments) {
 	template := authz0.FileToTemplate(filename)
-	_ = template
 	// TODO: Add scanning logic
+	for _, endpoint := range template.URLs {
+		_ = endpoint
+	}
 }
